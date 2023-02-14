4955d4cc41cea2de517bfb01046b4faaf173bdf1
Uladzislau Yorsh <vladyorsh@gmail.com>
2023-02-14 20:34:17 +0100

    working on MLM metrics

--- train.py
+++ train.py
@@ -26,7 +26,8 @@ is_encoder = args.model.lower() == 'encoder'
 def extend_with_rootdir(paths):
   return [ os.path.join(config.root_dir, p) for p in paths ]
 
-#Get the datasets first
+#*** GET DATA ***
+
 print('Parsing train dataset')
 train_dataset = NewsCrawlDataset(
     extend_with_rootdir(config.train_files), doc_split=True
@@ -42,7 +43,9 @@ if config.test_files:
       extend_with_rootdir(config.test_files), doc_split=True
   )
 
-#Get tokenizer, if cannot get -- train new
+#*** GET TOKENIZER ***
+#If cannot retrieve, train new
+
 tok_train_files = config.tokenizer_train_files
 if tok_train_files:
   tok_train_files = extend_with_rootdir(tok_train_files)
@@ -63,6 +66,8 @@ except:
   train_tokenizer(tokenizer, tok_train_data)
   tokenizer = make_fast_tokenizer(tokenizer)
   tokenizer.save_pretrained(tok_path)
+
+#*** ESTIMATE AVAILABLE MEMORY AND BATCH SIZES ***
 
 min_memory_available = 1000.0
 for device in range(torch.cuda.device_count()):
@@ -88,7 +93,8 @@ long_batch_size  = max(2, 2 ** round(math.log2(long_batch_size)))
 
 print(f'Estimated batch sizes: {short_batch_size} and {long_batch_size} for sentence and document level splits respectively')
 
-#Instantiate the model for training
+#*** CREATE MODEL ***
+
 model = HTransformer1D(config, is_encoder, tokenizer.pad_token_id, None)
 model = HFWrapper(model)
 short_collator = get_lm_collator(
@@ -99,6 +105,13 @@ long_collator = get_lm_collator(
   tokenizer, padding='longest', max_length=config.long_max_len,
   mask_prob=0.0 if not is_encoder else config.mlm_mask_prob
 )
+
+#def mlm_metrics(eval_preds):
+#  logits, labels = eval_preds
+
+#  return {}
+
+#*** DEFINE TRAINING ARGUMENTS ***
 
 common_args = {
   'do_train' : True, 'do_eval' : True, 'do_predict' : True if test_dataset else False,
@@ -128,6 +141,8 @@ long_training_args = TrainingArguments(
   ** common_args,
 )
 
+#*** SHORT SEQUENCE PRETRAINING ***
+
 TrainerClass = Trainer
 if config.short_eval_steps:
   short_training_args.max_eval_steps=config.short_eval_steps
@@ -141,6 +156,7 @@ short_trainer = TrainerClass(
   eval_dataset =valid_dataset,
 )
 
+#Set datasets to sentence-split mode for short-sequence pretraining
 train_dataset.doc_split = False
 valid_dataset.doc_split = False
 if test_dataset:
@@ -155,6 +171,8 @@ if args.long is None:
 else:
   print('*** Long checkpoint found, skipping short pretraining ***')
 
+#*** LONG SEQUENCE PRETRAINING
+
 TrainerClass = Trainer
 if config.long_eval_steps:
   long_training_args.max_eval_steps=config.long_eval_steps
@@ -168,6 +186,7 @@ long_trainer = TrainerClass(
   eval_dataset =valid_dataset,
 )
 
+#Set datasets to doc-split mode for long-sequence pretraining
 train_dataset.doc_split = True
 valid_dataset.doc_split = True
 if test_dataset:
